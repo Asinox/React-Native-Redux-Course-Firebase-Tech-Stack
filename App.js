f243b9973d7a0d7fe3f522f83763b1ae228995e1
Juan Miguel Calcaño <asinox@gmail.com>
2019-01-11 17:15:47 -0400

    working with redux

--- App.js
+++ App.js
@@ -8,11 +8,18 @@
 
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './src/reducers';
+
 
 export default class App extends Component {
   render() {
     return (
-      <View/>
+      <Provider store={createStore(reducers)}>
+        <View/>
+      </Provider>
     );
   }
 }
