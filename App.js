a8d23f30e6e079012d819adf1b9bf672a1d6d159
Juan Miguel Calcaño <asinox@gmail.com>
2019-01-14 17:06:28 -0400

    reducers

--- App.js
+++ App.js
@@ -6,19 +6,21 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import reducers from './src/reducers';
+import { Header } from "./src/components/common";
 
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View/>
+        <View>
+          <Header headerText = "Tech Stack" />
+        </View>
       </Provider>
     );
   }
